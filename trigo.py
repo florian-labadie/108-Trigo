550129f800d6308910b25c7df4ceb87e3c70e2ea
Axel <axel.lavrador@epitech.eu>
2024-03-18 16:32:59 +0100

    [ADD] print matrix

--- trigo.py
+++ trigo.py
@@ -1,19 +1,29 @@
+def check_equals(m1, m2):
+    for l1, l2 in m1, m2:
+        for i1, i2 in l1, l2:
+            if (i1 != i2):
+                return 84
+    return 0
+
 def exp(matrix):
     
     return 0
 
-def cos(n):
+def cos(matrix):
     
     return 0
 
-def sin(n):
+def sin(matrix):
     return 0
 
-def cosh(n):
+def cosh(matrix):
     return 0
 
-def sinh(n):
+def sinh(matrix):
     return 0
+
+def mat_mul(matrix, matrix2):
+    return [[sum(matrix[i][k] * matrix2[k][j] for k in range(len(matrix2))) for j in range(len(matrix2[0]))] for i in range(len(matrix))]
 
 def trigo(function, matrix):
     if (function == "COS"):
@@ -26,4 +36,5 @@ def trigo(function, matrix):
         sinh(matrix)
     elif (function == "EXP"):
         exp(matrix)
+    print('\n'.join("\t".join(f"{x:.2f}" if x != 0 else "0.00" for x in line) for line in matrix))
     return 0
